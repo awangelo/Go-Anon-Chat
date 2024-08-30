c5dde21767be0704fb9965a2d929db20001067cd
awangelo <anggg.gaio@gmail.com>
2024-08-30 18:35:53 -0300

    alterando funcao de numUsers para teste

--- internal/chat/handler.go
+++ internal/chat/handler.go
@@ -6,30 +6,15 @@ import (
 	"net/http"
 )
 
-// Pode ser apenas um map por enquanto
-// type chatServer struct {
-var subscribers map[*subscriber]struct{}
-
-// Adcionar tempo limite? (1 dia)
-// }
-
-type subscriber struct {
-	ip    string
-	color int
-}
-
 // IndexHandler aplica o numero de users conectados no template principal.
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	numUsers := getNumberOfUsers()
+	numUsers := getNumberOfSubscribers()
 
+	// ParseFiles retorna o template do chat com o numero de subscribers.
 	tmpl := template.Must(template.ParseFiles("web/index.html"))
 	err := tmpl.Execute(w, numUsers)
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		log.Fatal("Error executing template:", err)
 	}
-}
-
-func getNumberOfUsers() int {
-	return 2
 }
