02556f45fcebacd2b63ef716dd9f8eef53cb756b
awangelo <anggg.gaio@gmail.com>
2024-09-06 10:25:30 -0300

    removido contador de usuarios falso

--- internal/chat/handler.go
+++ internal/chat/handler.go
@@ -8,11 +8,9 @@ import (
 
 // IndexHandler aplica o numero de users conectados no template principal.
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	numUsers := getNumberOfSubscribers()
-
 	// ParseFiles retorna o template do chat com o numero de subscribers.
 	tmpl := template.Must(template.ParseFiles("web/index.html"))
-	err := tmpl.Execute(w, numUsers)
+	err := tmpl.Execute(w, nil)
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		log.Fatal("Error executing template:", err)
