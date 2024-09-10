81d9d3122a161b90e48d2838f90dea0fb2302b99
awangelo <anggg.gaio@gmail.com>
2024-09-10 10:56:55 -0300

    funcao getUserIdentity para gerar identificador e cor usada por CreateSubscriber que retorna um novo usuario conectado

--- internal/chat/subscriber.go
+++ internal/chat/subscriber.go
@@ -1,15 +1,44 @@
 package chat
 
-import "fmt"
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"net"
+	"strings"
+)
 
 type Subscriber struct {
 	ip    string
-	color int
+	color string
 	// As mensagens de cada subscriber sao enviadas atraves deste canal.
 	send chan []byte
 }
 
-// Apenas para ver o para de *Subscribers
-func (s *Subscriber) String() string {
-	return fmt.Sprintf("ip: %v, color: %v", s.ip, s.color)
+// createSubscriber cria um novo subscriber com base no endereco IP.
+func CreateSubscriber(remoteAddr string) *Subscriber {
+	identifier, userColor := getUserIdentity(remoteAddr)
+	return &Subscriber{
+		ip:    identifier,
+		color: userColor,
+		send:  make(chan []byte, 256),
+	}
+}
+
+// getUserIdentity retorna um identificador e uma cor unica para um endereco IP.
+func getUserIdentity(remoteAddr string) (string, string) {
+	ip, _, _ := net.SplitHostPort(remoteAddr)
+
+	octetos := strings.Split(ip, ".")
+	identifier := fmt.Sprintf(".%s..%s", octetos[1], octetos[3])
+
+	color := getColorFromIP(ip)
+
+	return identifier, color
+}
+
+// getColorFromIP retorna uma cor hexadecimal unica com base no endereco IP.
+func getColorFromIP(ip string) string {
+	hash := md5.Sum([]byte(ip))
+	return "#" + hex.EncodeToString(hash[:3])
 }
