fa7ea0ad0acbb381c125abbbd45dda20731ef07f
awangelo <anggg.gaio@gmail.com>
2024-09-10 11:00:14 -0300

    handler do index simplificado

--- internal/chat/handler.go
+++ internal/chat/handler.go
@@ -1,18 +1,10 @@
 package chat
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 )
 
 // IndexHandler aplica o numero de users conectados no template principal.
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	// ParseFiles retorna o template do chat com o numero de subscribers.
-	tmpl := template.Must(template.ParseFiles("web/index.html"))
-	err := tmpl.Execute(w, nil)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		log.Fatal("Error executing template:", err)
-	}
+	http.ServeFile(w, r, "web/index.html")
 }
